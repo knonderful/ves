40c1fd0b8dfb32d553b0f1607d2b811532d2a5cb
knonderful <knonderful@hotmail.com>
2021-03-20 15:54:20 +0100

    Added mem module with RomBlock as a first type.

--- prototype/proto-common/src/lib.rs
+++ prototype/proto-common/src/lib.rs
@@ -1,3 +1,4 @@
 #[macro_use]
 mod util;
 pub mod gpu;
+pub mod mem;
---
+++ prototype/proto-common/src/mem.rs
@@ -0,0 +1,20 @@
+bit_struct!(
+    /// A contiguous block of memory in the game ROM.
+    ///
+    /// The internal format is as follows:
+    /// * Bits 0-31: The offset.
+    /// * Bits 32-63: The length.
+    pub struct RomBlock {
+        value: u64
+    }
+
+    impl {
+        #[bit_struct_field(shift = 0, mask = 0xFFFFFFFF)]
+        /// The offset to the first byte of the block inside the ROM.
+        pub fn offset(&self) -> u32;
+
+        #[bit_struct_field(shift = 32, mask = 0xFFFFFFFF)]
+        /// The length of the block.
+        pub fn len(&self) -> u32;
+    }
+);

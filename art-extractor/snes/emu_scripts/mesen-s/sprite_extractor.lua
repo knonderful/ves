f2e142c309f55d87f842bfdeaeba6ca5fe021ee3
knonderful <knonderful@hotmail.com>
2021-12-06 23:05:03 +0100

    Added utility function for printing tables to LUA script.

--- art-extractor/snes/emu_scripts/mesen-s/sprite_extractor.lua
+++ art-extractor/snes/emu_scripts/mesen-s/sprite_extractor.lua
@@ -395,6 +395,58 @@ end
 ========== END COPIED JSON.LUA
 ]]--
 
+
+--[[
+-- Utility function to print a LUA table.
+-- t = table
+-- f = filename [optional]
+function printTable(t, f)
+
+   local function printTableHelper(obj, cnt)
+
+      local cnt = cnt or 0
+
+      if type(obj) == "table" then
+
+         io.write("\n", string.rep("\t", cnt), "{\n")
+         cnt = cnt + 1
+
+         for k,v in pairs(obj) do
+
+            if type(k) == "string" then
+               io.write(string.rep("\t",cnt), '["'..k..'"]', ' = ')
+            end
+
+            if type(k) == "number" then
+               io.write(string.rep("\t",cnt), "["..k.."]", " = ")
+            end
+
+            printTableHelper(v, cnt)
+            io.write(",\n")
+         end
+
+         cnt = cnt-1
+         io.write(string.rep("\t", cnt), "}")
+
+      elseif type(obj) == "string" then
+         io.write(string.format("%q", obj))
+
+      else
+         io.write(tostring(obj))
+      end
+   end
+
+   if f == nil then
+      printTableHelper(t)
+   else
+      io.output(f)
+      io.write("return")
+      printTableHelper(t)
+      io.output(io.stdout)
+   end
+end
+]]--
+
 grabbed = false;
 
 function grabSpriteData()

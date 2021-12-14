29fe60315f034747b70f267679e1c54ea8256d9f
knonderful <knonderful@hotmail.com>
2021-12-14 23:18:28 +0100

    Replaced pixel_iterator() with SurfaceIter.

--- art-extractor/core/src/surface.rs
+++ art-extractor/core/src/surface.rs
@@ -1,7 +1,6 @@
 //! A module for working with 2-dimensional surfaces.
 
-use std::ops::Range;
-use generator::done;
+use std::ops::{Range, RangeInclusive};
 use crate::geom::{ArtworkSpaceUnit, Point, Rect, Size};
 
 /// Local trait for extending `ArtworkSpaceUnit` with `into_usize()`.
@@ -215,69 +214,340 @@ mod test_surface_row_iter {
     }
 }
 
-pub fn pixel_iterator(rect: Rect, hflip: bool, vflip: bool) -> impl Iterator<Item=Point> {
-    macro_rules! create_iterator {
-        ($x_method:expr, $y_method:expr) => {
-            generator::Gn::new_scoped_local(move |mut scope| {
-                for y in $y_method {
-                    for x in $x_method {
-                        scope.yield_(Point::new(x, y));
-                    }
-                }
-                done!();
-            })
-        }
+pub trait AxisIterFactory {
+    type IterType: Iterator<Item=usize>;
+
+    fn new_iter(min: usize, max: usize) -> Self::IterType;
+}
+
+pub struct ForwardIteration;
+
+pub struct ReverseIteration;
+
+impl AxisIterFactory for ForwardIteration {
+    type IterType = RangeInclusive<usize>;
+
+    fn new_iter(min: usize, max: usize) -> Self::IterType {
+        min..=max
+    }
+}
+
+impl AxisIterFactory for ReverseIteration {
+    type IterType = std::iter::Rev<RangeInclusive<usize>>;
+
+    fn new_iter(min: usize, max: usize) -> Self::IterType {
+        ForwardIteration::new_iter(min, max).rev()
+    }
+}
+
+/// A convenience macro for creating a [`SurfaceIter`].
+#[macro_export]
+macro_rules! surface_iter {
+    ($size:expr, $rect:expr, @hflip, @vflip) => {
+        $crate::surface::SurfaceIter::<$crate::surface::ReverseIteration, $crate::surface::ReverseIteration>::new($size, $rect)
+    };
+    ($size:expr, $rect:expr, @hflip) => {
+        $crate::surface::SurfaceIter::<$crate::surface::ReverseIteration, $crate::surface::ForwardIteration>::new($size, $rect)
+    };
+    ($size:expr, $rect:expr, @vflip) => {
+        $crate::surface::SurfaceIter::<$crate::surface::ForwardIteration, $crate::surface::ReverseIteration>::new($size, $rect)
+    };
+    ($size:expr, $rect:expr) => {
+        $crate::surface::SurfaceIter::<$crate::surface::ForwardIteration, $crate::surface::ForwardIteration>::new($size, $rect)
+    };
+}
+
+pub struct SurfaceIter<X, Y> where
+    X: AxisIterFactory,
+    Y: AxisIterFactory,
+{
+    pitch: usize,
+    x_min: usize,
+    x_max: usize,
+    x_iter: X::IterType,
+    y_iter: Y::IterType,
+    row_offset: usize,
+}
+
+impl<X, Y> SurfaceIter<X, Y> where
+    X: AxisIterFactory,
+    Y: AxisIterFactory,
+{
+    pub fn new(size_surf: Size, rect_view: Rect) -> Self {
+        let pitch = size_surf.width.into_usize();
+        let x_min = rect_view.min_x().into_usize();
+        let x_max = rect_view.max_x().into_usize();
+        let x_iter = X::new_iter(x_min, x_max);
+        let mut y_iter = Y::new_iter(rect_view.min_y().into_usize(), rect_view.max_y().into_usize());
+        let row_offset = y_iter.next().expect("Expected at least one item in Y-iterator.") * pitch;
+        Self { pitch, x_min, x_max, x_iter, y_iter, row_offset }
     }
 
-    match (hflip, vflip) {
-        (false, false) => create_iterator!(rect.range_x(), rect.range_y()),
-        (true, false) => create_iterator!(rect.range_x().rev(), rect.range_y()),
-        (false, true) => create_iterator!(rect.range_x(), rect.range_y().rev()),
-        (true, true) => create_iterator!(rect.range_x().rev(), rect.range_y().rev()),
+    #[inline(always)]
+    fn do_next(&mut self) -> Option<usize> {
+        match self.x_iter.next() {
+            Some(x) => Some(self.row_offset + (x % self.pitch)),
+            None => {
+                match self.y_iter.next() {
+                    None => None,
+                    Some(y) => {
+                        self.row_offset = y * self.pitch;
+                        self.x_iter = X::new_iter(self.x_min, self.x_max);
+                        self.do_next()
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<X, Y> Iterator for SurfaceIter<X, Y> where
+    X: AxisIterFactory,
+    Y: AxisIterFactory,
+{
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.do_next()
     }
 }
 
 #[cfg(test)]
-mod test_module_functions {
-    use crate::geom::generic::Point;
-    use crate::surface::pixel_iterator;
+mod test_surface_iter {
+    use crate::geom::{Rect, Size};
+    use crate::sprite::GenericSurface;
+    use crate::surface::Surface;
 
-    #[test]
-    fn test_pixel_iterator() {
-        let rect = ((8, 16), 4, 10).into();
+    type Surfy = GenericSurface<u8>;
 
-        let mut iter = pixel_iterator(rect, false, false);
-        for y in rect.range_y() {
-            for x in rect.range_x() {
-                assert_eq!(Point::new(x, y), iter.next().unwrap());
-            }
+    macro_rules! data {
+        ($($elt:expr)*) => {
+            [ $($elt,)* ]
         }
-        assert_eq!(None, iter.next());
-
-        let mut iter = pixel_iterator(rect, true, false);
-        for y in rect.range_y() {
-            for x in rect.range_x().rev() {
-                assert_eq!(Point::new(x, y), iter.next().unwrap());
-            }
-        }
-        assert_eq!(None, iter.next());
-
-        let mut iter = pixel_iterator(rect, false, true);
-        for y in rect.range_y().rev() {
-            for x in rect.range_x() {
-                assert_eq!(Point::new(x, y), iter.next().unwrap());
-            }
-        }
-        assert_eq!(None, iter.next());
-
-        let mut iter = pixel_iterator(rect, true, true);
-        for y in rect.range_y().rev() {
-            for x in rect.range_x().rev() {
-                assert_eq!(Point::new(x, y), iter.next().unwrap());
-            }
-        }
-        assert_eq!(None, iter.next());
     }
+
+    const SOURCE_DATA: [u8; 12 * 8] = data![
+        0 0 0 1 1 1 1 1 0 0 0 0
+        0 0 1 1 1 1 1 1 1 1 1 0
+        0 0 2 2 2 3 3 2 3 0 0 0
+        0 2 3 2 3 3 3 2 3 3 3 0
+        0 2 3 2 2 3 3 3 2 3 3 3
+        0 2 2 3 3 3 3 2 2 2 2 0
+        0 0 0 3 3 3 3 3 3 3 0 0
+        0 0 1 1 4 1 1 1 1 0 0 0
+    ];
+    const EMPTY_DATA: [u8; 12 * 8] = data![
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0
+    ];
+
+    fn copy_data(src_surf: &Surfy, dest_surf: &mut Surfy, src_iter: impl Iterator<Item=usize>, dest_iter: impl Iterator<Item=usize>) {
+        let src = src_surf.data();
+
+        let dest = dest_surf.data_mut();
+        for (src_idx, dest_idx) in src_iter.zip(dest_iter) {
+            //println!("src: {} dest: {}", src_idx, dest_idx);
+            dest[dest_idx] = src[src_idx];
+        }
+    }
+
+    fn create_source() -> Surfy {
+        let mut src = Surfy::new(Size::new(12, 8), 0);
+        assert_eq!(&EMPTY_DATA, src.data());
+
+        src.data_mut().copy_from_slice(&SOURCE_DATA);
+        assert_eq!(&SOURCE_DATA, src.data());
+        src
+    }
+
+    /// Test with a copy of the entire source surface.
+    #[test]
+    fn test_full_copy() {
+        // No flipping
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()));
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()));
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&SOURCE_DATA, dest.data());
+        }
+
+        // H-flip on both
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @hflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @hflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&SOURCE_DATA, dest.data());
+        }
+
+        // V-flip on both
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @vflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @vflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&SOURCE_DATA, dest.data());
+        }
+
+        // H-flip and v-flip on both
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @hflip, @vflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @hflip, @vflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&SOURCE_DATA, dest.data());
+        }
+    }
+
+    /// Test with a copy of the entire source surface using horizontal flipping.
+    #[test]
+    fn test_full_copy_hflip() {
+        const EXPECTED: [u8; 12 * 8] = data![
+            0 0 0 0 1 1 1 1 1 0 0 0
+            0 1 1 1 1 1 1 1 1 1 0 0
+            0 0 0 3 2 3 3 2 2 2 0 0
+            0 3 3 3 2 3 3 3 2 3 2 0
+            3 3 3 2 3 3 3 2 2 3 2 0
+            0 2 2 2 2 3 3 3 3 2 2 0
+            0 0 3 3 3 3 3 3 3 0 0 0
+            0 0 0 1 1 1 1 4 1 1 0 0
+        ];
+
+        // H-flip on src
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @hflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()));
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+
+        // H-flip on dest
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()));
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @hflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+    }
+
+    /// Test with a copy of the entire source surface using vertical flipping.
+    #[test]
+    fn test_full_copy_vflip() {
+        const EXPECTED: [u8; 12 * 8] = data![
+            0 0 1 1 4 1 1 1 1 0 0 0
+            0 0 0 3 3 3 3 3 3 3 0 0
+            0 2 2 3 3 3 3 2 2 2 2 0
+            0 2 3 2 2 3 3 3 2 3 3 3
+            0 2 3 2 3 3 3 2 3 3 3 0
+            0 0 2 2 2 3 3 2 3 0 0 0
+            0 0 1 1 1 1 1 1 1 1 1 0
+            0 0 0 1 1 1 1 1 0 0 0 0
+        ];
+
+        // V-flip on src
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @vflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()));
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+
+        // V-flip on dest
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()));
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @vflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+    }
+
+
+    /// Test with a copy of the entire source surface using both horizontal and vertical flipping.
+    #[test]
+    fn test_full_copy_hflip_vflip() {
+        const EXPECTED: [u8; 12 * 8] = data![
+            0 0 0 1 1 1 1 4 1 1 0 0
+            0 0 3 3 3 3 3 3 3 0 0 0
+            0 2 2 2 2 3 3 3 3 2 2 0
+            3 3 3 2 3 3 3 2 2 3 2 0
+            0 3 3 3 2 3 3 3 2 3 2 0
+            0 0 0 3 2 3 3 2 2 2 0 0
+            0 1 1 1 1 1 1 1 1 1 0 0
+            0 0 0 0 1 1 1 1 1 0 0 0
+        ];
+
+        // H-flip and v-flip on src
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @hflip, @vflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()));
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+
+        // H-flip and v-flip on dest
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()));
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @hflip, @vflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+
+        // H-flip on src and v-flip on dest
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @hflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @vflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+
+
+        // H-flip on dest and v-flip on src
+        {
+            let src = create_source();
+            let mut dest = Surfy::new(src.size(), 0);
+            let src_iter = surface_iter!(src.size(), Rect::new((0, 0).into(), src.size()), @vflip);
+            let dest_iter = surface_iter!(dest.size(), Rect::new((0, 0).into(), dest.size()), @hflip);
+            copy_data(&src, &mut dest, src_iter, dest_iter);
+            assert_eq!(&EXPECTED, dest.data());
+        }
+    }
+
+    // TODO: Tests:
+    // * Entire surface copy.
+    // * View inside surface.
+    // * View that extends over X.
+    // * View that extends over Y.
+    // * View that extends over X and Y.
+    // * For all the above: all iteration types.
+    // * View that is too big in X, Y and both. (view > surface in some dimension)
+    // TIP: Use a simple format like 10x5, that is easy to write expected values in... use simple surface format like u8 or something
+}
+
 pub struct PixelIter<X, Y> {
     x_iter: X,
     y_iter: Y,
@@ -308,7 +578,7 @@ impl<X, Y> PixelIter<X, Y> where
                         self.do_next()
                     }
                 }
-            },
+            }
         }
     }
 }
@@ -366,5 +636,4 @@ mod test_pixel_iter {
         }
         assert_eq!(None, iter.next());
     }
-
 }

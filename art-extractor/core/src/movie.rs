b730f3cf10ad005b2c8e6cf67951408ae8640277
knonderful <knonderful@hotmail.com>
2022-01-06 21:28:34 +0100

    Added getters to movie types.

--- art-extractor/core/src/movie.rs
+++ art-extractor/core/src/movie.rs
@@ -32,6 +32,31 @@ impl Movie {
     pub fn new(screen_size: Size, palettes: Vec<Palette>, tiles: Vec<Tile>, frames: Vec<MovieFrame>, frame_rate: FrameRate) -> Self {
         Self { screen_size, palettes, tiles, frames, frame_rate }
     }
+
+    /// Retrieves the screen size.
+    pub fn screen_size(&self) -> Size {
+        self.screen_size
+    }
+
+    /// Retrieves the palettes.
+    pub fn palettes(&self) -> &[Palette] {
+        &self.palettes
+    }
+
+    /// Retrieves the tiles.
+    pub fn tiles(&self) -> &[Tile] {
+        &self.tiles
+    }
+
+    /// Retrieves the frames.
+    pub fn frames(&self) -> &[MovieFrame] {
+        &self.frames
+    }
+
+    /// Retrieves the frame rate.
+    pub fn frame_rate(&self) -> FrameRate {
+        self.frame_rate
+    }
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -45,5 +70,15 @@ impl MovieFrame {
     /// Creates a new instance.
     pub fn new(frame_number: u64, sprites: Vec<Sprite>) -> Self {
         Self { frame_number, sprites }
+    }
+
+    /// Retrieves the frame number.
+    pub fn frame_number(&self) -> u64 {
+        self.frame_number
+    }
+
+    /// Retrieves the sprites.
+    pub fn sprites(&self) -> &[Sprite] {
+        &self.sprites
     }
 }

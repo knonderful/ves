61058080db3ae65b2d449d015afb5387cec4bcad
knonderful <knonderful@hotmail.com>
2022-01-07 00:21:38 +0100

    Added test for create_movie() with some debug output options.

--- art-extractor/snes/src/lib.rs
+++ art-extractor/snes/src/lib.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use art_extractor_core::geom_art::Size;
 use art_extractor_core::movie::{FrameRate, Movie};
 use ves_cache::IndexedCache;
@@ -9,24 +9,8 @@ pub(crate) mod test_util;
 mod mesen;
 mod obj;
 
-fn json_files(directory: &impl AsRef<Path>) -> std::io::Result<Vec<PathBuf>> {
-    let mut paths = Vec::new();
-    for dir_entry_result in std::fs::read_dir(directory)? {
-        let path = dir_entry_result?.path();
-        if path.is_file() && path.ends_with(".json") {
-            paths.push(path);
-        }
-    }
-    Ok(paths)
-}
-
-/// Creates a [`Movie`] from the Mesen-S export files in the provided directory.
-pub fn create_movie(directory: impl AsRef<Path>) -> anyhow::Result<Movie> {
-    // Get all files, filter on JSON
-    let mut files = json_files(&directory)?;
-    // Sort by file name
-    files.sort_unstable();
-
+/// Creates a [`Movie`] from the provided Mesen-S export files.
+pub fn create_movie(files: Vec<impl AsRef<Path>>) -> anyhow::Result<Movie> {
     let mut palettes = IndexedCache::new();
     let mut tiles = IndexedCache::new();
 
@@ -40,4 +24,61 @@ pub fn create_movie(directory: impl AsRef<Path>) -> anyhow::Result<Movie> {
 
     let movie = Movie::new(Size::new(512.into(), 256.into()), palettes.consume(), tiles.consume(), movie_frames, FrameRate::Ntsc);
     Ok(movie)
+}
+
+#[cfg(test)]
+mod test_create_movie {
+    use std::borrow::Cow;
+    use std::fs::File;
+    use art_extractor_core::movie::Movie;
+    use ves_cache::IndexedCache;
+    use super::create_movie;
+
+    #[test]
+    fn test_full() {
+        let mut input_frames_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        input_frames_dir.push("resources/test/mesen-s_frames");
+
+        const NR_OF_FRAMES: usize = 10;
+
+        let mut files = Vec::with_capacity(NR_OF_FRAMES);
+        for frame in 0..NR_OF_FRAMES {
+            files.push(input_frames_dir.join(format!("frame_{}.json", 199250 + frame)));
+        }
+
+        let actual_movie = create_movie(files).unwrap();
+        let mut palettes = IndexedCache::new();
+        let mut tiles = IndexedCache::new();
+
+        // TODO: Change test_util::bmp_from_movie_frame to accept something more lenient than an IndexedCache, so that we don't have to do this
+        for palette in actual_movie.palettes() {
+            palettes.offer(Cow::Borrowed(palette));
+        }
+        for tile in actual_movie.tiles() {
+            tiles.offer(Cow::Borrowed(tile));
+        }
+
+        const DEBUG_OUT: bool = false;
+        if DEBUG_OUT {
+            for frame in actual_movie.frames() {
+                let actual = crate::test_util::bmp_from_movie_frame(&frame, &palettes, &tiles);
+                actual.save(format!("{}/../../target/movie_frame_{}.bmp", env!("CARGO_MANIFEST_DIR"), frame.frame_number())).unwrap();
+            }
+
+            let bincode_file = File::create(format!("{}/../../target/movie_{}_frames.bincode", env!("CARGO_MANIFEST_DIR"), NR_OF_FRAMES)).unwrap();
+            bincode::serialize_into(bincode_file, &actual_movie).unwrap();
+
+            let json_file = File::create(format!("{}/../../target/movie_{}_frames.json", env!("CARGO_MANIFEST_DIR"), NR_OF_FRAMES)).unwrap();
+            serde_json::to_writer(json_file, &actual_movie).unwrap();
+            // Alternatively:
+            // serde_json::to_writer_pretty(json_file, &movie).unwrap();
+        }
+
+        let mut expected_movie_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        expected_movie_path.push(format!("resources/test/expected/movie_{}_frames.bincode", NR_OF_FRAMES));
+        let expected_movie_file = File::open(expected_movie_path).unwrap();
+        let expected_movie: Movie = bincode::deserialize_from(expected_movie_file).unwrap();
+
+        assert_eq!(expected_movie, actual_movie);
+    }
 }

4a94c22b4330bec0efcd6855d9854e4e141ee991
knonderful <knonderful@hotmail.com>
2022-01-10 22:27:41 +0100

    Added implementations for Zero and One.

--- geom/src/lib.rs
+++ geom/src/lib.rs
@@ -7,11 +7,55 @@ pub trait Zero {
     fn zero() -> Self;
 }
 
+macro_rules! impl_zero {
+    ($ty:ty) => {
+        impl Zero for $ty {
+            fn zero() -> Self {
+                0
+            }
+        }
+
+    }
+}
+
+impl_zero!(u8);
+impl_zero!(u16);
+impl_zero!(u32);
+impl_zero!(u64);
+impl_zero!(usize);
+impl_zero!(i8);
+impl_zero!(i16);
+impl_zero!(i32);
+impl_zero!(i64);
+impl_zero!(isize);
+
 /// Returns the value one (1) for a type.
 pub trait One {
     /// Returns the value one.
     fn one() -> Self;
 }
+
+macro_rules! impl_one {
+    ($ty:ty) => {
+        impl One for $ty {
+            fn one() -> Self {
+                1
+            }
+        }
+
+    }
+}
+
+impl_one!(u8);
+impl_one!(u16);
+impl_one!(u32);
+impl_one!(u64);
+impl_one!(usize);
+impl_one!(i8);
+impl_one!(i16);
+impl_one!(i32);
+impl_one!(i64);
+impl_one!(isize);
 
 /// A unit that is used in a geometrical "space".
 ///

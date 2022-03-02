0243dfb4e494a3fa44cbc8ba0da0dca7ce5f6cc4
knonderful <knonderful@hotmail.com>
2022-03-02 14:27:42 +0100

    Renamed and documented Core logging function.

--- proto/game/src/lib.rs
+++ proto/game/src/lib.rs
@@ -8,13 +8,22 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[link(wasm_import_module = "log")]
 extern "C" {
+    /// Core function for logging.
+    ///
+    /// This function pointer is intended to be passed into a [`Logger`] instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `level`: The [`LogLevel`](ves_proto_common::log::LogLevel).
+    /// * `ptr`: A pointer to the start of the message.
+    /// * `len`: The length of the message in bytes.
     #[link_name = "log"]
-    fn log_fn(level: u32, ptr: *const u8, len: usize);
+    fn core_log_log(level: u32, ptr: *const u8, len: usize);
 }
 
 #[no_mangle]
 pub fn create_instance() -> Box<Game> {
-    Logger::new(log_fn).init(Some(LogLevel::Trace)).unwrap();
+    Logger::new(core_log_log).init(Some(LogLevel::Trace)).unwrap();
     info!("Logging initialized.");
 
     Box::new(Game { frame_nr: 1024 })
